e840dbb26af1acbe3f1a20674594fdafae1faf35
daffaoktaa001 <soundmax.id@gmail.com>
2023-01-06 14:08:15 +0700

    IuranWargaPBO

---
+++ IuranWarga.java
@@ -0,0 +1,36 @@
+package IuranWarga;
+public class IuranWarga {
+    Integer NIK,Sampah,Keamanan,Sumbangan;
+
+    public Integer getNIK() {
+        return NIK;
+    }
+
+    public void setNIK(Integer NIK) {
+        this.NIK = NIK;
+    }
+
+    public Integer getSampah() {
+        return Sampah;
+    }
+
+    public void setSampah(Integer Sampah) {
+        this.Sampah = Sampah;
+    }
+
+    public Integer getKeamanan() {
+        return Keamanan;
+    }
+
+    public void setKeamanan(Integer Keamanan) {
+        this.Keamanan = Keamanan;
+    }
+
+    public Integer getSumbangan() {
+        return Sumbangan;
+    }
+
+    public void setSumbangan(Integer Sumbangan) {
+        this.Sumbangan = Sumbangan;
+    }
+}
